d3b5653fb61a6a2c21ee4742f4ef56cce6f83bd4
Nickson Bett <kiprotichbettn@gmail.com>
2026-04-12 07:24:24 -0400

    Initial commit: C# console app with array and foreach loop

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+string techStream = "AI,Machine Learning,Web Development,Cloud Computing,Cybersecurity";
+string[] technologies = techStream.Split(',');
+
+Array.Sort(technologies);
+
+foreach (var tech in technologies)
+{
+    if (tech.Contains(" "))
+    {
+        Console.WriteLine($"{tech}\t- Multi-word");
+    }
+    else
+    {
+        Console.WriteLine($"{tech}\t- Single-word");
+    }
+}
